9952ea9f05df03b678432a4a57a166ab085e14a1
unknown <nickroweb@gmail.com>
2022-05-24 23:31:47 +1000

    added files

---
+++ fonts/header-buttons.js
@@ -0,0 +1,72 @@
+
+function setBurger(params) {
+  const btn = document.querySelector(`.${params.btnClass}`);
+  const menu = document.querySelector(`.${params.menuClass}`);
+
+  menu.addEventListener("animationend", function () {
+    if (this.classList.contains(params.hiddenClass)) {
+      this.classList.remove(params.activeClass);
+      this.classList.remove(params.hiddenClass);
+    }
+  });
+
+  btn.addEventListener("click", function () {
+    this.classList.toggle(params.activeClass);
+
+    if (
+      !menu.classList.contains(params.activeClass) &&
+      !menu.classList.contains(params.hiddenClass)
+    ) {
+      menu.classList.add(params.activeClass);
+      document.body.style.overflow = 'hidden';
+    } else {
+      menu.classList.add(params.hiddenClass);
+      document.body.removeAttribute('style');
+    }
+  });
+}
+
+// здесь мы вызываем функцию и передаем в нее классы наших элементов
+setBurger({
+  btnClass: "burger-button", // класс бургера
+  menuClass: "header__menu-block", // класс меню
+  activeClass: "is-opened", // класс открытого состояния
+  hiddenClass: "is-closed" // класс закрывающегося состояния (удаляется сразу после закрытия)
+});
+
+function setSearch(params) {
+  const openBtn = document.querySelector(`.${params.openBtnClass}`);
+  const search = document.querySelector(`.${params.searchClass}`);
+  const closeBtn = search.querySelector(`.${params.closeBtnClass}`);
+
+  search.addEventListener("animationend", function () {
+    if (this.classList.contains(params.hiddenClass)) {
+      this.classList.remove(params.activeClass);
+      this.classList.remove(params.hiddenClass);
+    }
+  });
+
+  openBtn.addEventListener("click", function (evt) {
+    this.disabled = true;
+
+    if (
+      !search.classList.contains(params.activeClass) &&
+      !search.classList.contains(params.hiddenClass)
+    ) {
+      search.classList.add(params.activeClass);
+    }
+  });
+
+  closeBtn.addEventListener('click', function () {
+    openBtn.disabled = false;
+    search.classList.add(params.hiddenClass);
+  });
+}
+
+setSearch({
+  openBtnClass: "header__search-button", // класс кнопки открытия
+  closeBtnClass: "close-btn", // класс кнопки закрытия
+  searchClass: "search-form", // класс формы поиска
+  activeClass: "is-opened", // класс открытого состояния
+  hiddenClass: "is-closed" // класс закрывающегося состояния (удаляется сразу после закрытия)
+});
